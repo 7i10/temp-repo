781c9c41e0bdc6a82bd3a2029459d27943d35a6c
ctlei2 <ctlei2@144.214.209.211>
2025-04-20 21:03:03 +0800

    first commit

---
+++ archs.py
@@ -0,0 +1,34 @@
+from dataset import get_normalize_layer
+import torchvision
+from transformers import BeitForImageClassification
+import torch
+
+
+def get_archs(arch, dataset='imagenet'):
+    if dataset == 'imagenet':
+        if arch == 'resnet50':
+            model = torchvision.models.resnet50(weights="DEFAULT")
+        
+        elif arch == 'resnet152':
+            model = torchvision.models.resnet152(weights="DEFAULT")
+            
+        elif arch == 'wrn50':
+            model = torchvision.models.wide_resnet50_2(weights="DEFAULT")
+
+        elif arch == 'vit':
+            model = torchvision.models.vit_b_16(weights='DEFAULT')
+
+        elif arch == 'swin_b':
+            model = torchvision.models.swin_b(pretrained=True)
+
+        elif arch == 'convnext_b':
+            model = torchvision.models.convnext_base(pretrained=True)
+            
+        elif arch == 'beit':
+            model = BeitForImageClassification.from_pretrained('microsoft/beit-large-patch16-224')
+    
+    normalize_layer = get_normalize_layer(dataset, vit=True if ("vit" in arch or 'beit' in arch) else False)
+    
+    return torch.nn.Sequential(normalize_layer, model)
+
+    
