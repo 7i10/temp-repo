39b915e5fcd25c9021c97552bbb976ccb36312c7
antony090 <89074769+antony090@users.noreply.github.com>
2025-05-20 17:19:42 +0800

    Update train_lora.py

--- train_lora.py
+++ train_lora.py
@@ -13,6 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 
+# We modify on top of LCM-LoRA's code
+
 import os
 import math
 import shutil
