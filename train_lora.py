2cd8baa6c6aaad9e6005268c49e4a112a4cb8219
antony090 <89074769+antony090@users.noreply.github.com>
2025-05-20 17:24:39 +0800

    Update train_lora.py

--- train_lora.py
+++ train_lora.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 
-# We modify on top of LCM-LoRA's code
+# We modify on top of https://github.com/luosiallen/latent-consistency-model
 
 import os
 import math
